4030d784c389361328ee2e25710a79a0521e06c1
Sinan Kanidagli <sinan@knid.me>
2025-05-14 16:53:30 +0300

    changed head informations

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Timezen",
+  description: "Build productive sub-tasks",
   navItems: [
     {
       label: "Home",
